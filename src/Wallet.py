df997075057f9e2a5ec53898bd4bad429f4801e1
trekimann <10469075+trekimann@users.noreply.github.com>
2023-01-08 21:52:58 +0000

    add give change and remove money method

--- src/Wallet.py
+++ src/Wallet.py
@@ -38,6 +38,15 @@ class Wallet:
         else:
             self.money[money.name] = {'item':money,'count':count}
         return True
+
+    def remove_money(self, money, count = 1):
+        if money.name in self.money.keys():
+            self.money[money.name]['count'] -= count
+            if self.money[money.name]['count'] <= 0:
+                del self.money[money.name]
+            return True
+        else:
+            return False
     
     def pay_money(self, amount, target_wallet):
     # Sort the money in the wallet by value in descending order
@@ -65,3 +74,48 @@ class Wallet:
             # If the desired amount has been reached, break out of the loop
             if total_paid == amount:
                 break
+        if total_paid != amount:
+            #get the smallest denomination which would cover the remaining amount and get change for it from the other wallet, then pay the rest of the amount
+            remaining_amount = amount - total_paid
+            for currency in sorted_money:
+                money = currency[1]['item']
+                money_value = self.money[money.name]['item'].value
+                if money_value >= remaining_amount:
+                    target_wallet.give_change(money, self)
+                    self.pay_money(remaining_amount, target_wallet)
+                    break
+
+    def give_change(self, money_to_break, target_wallet):
+        # This method takes in a money object then breaks it down into the smallest possible denominations that the wallet holds and returns them to the target wallet
+
+        # Sort the money in the wallet by value in descending order
+        sorted_money = sorted(self.money.items(), key=lambda x: x[1]['item'].value, reverse=False)
+
+        # Initialize a counter for the total value of money paid
+        total_paid = 0
+
+        # get the money to break downs value
+        amount = money_to_break.value
+
+        # Iterate through the sorted money
+        for currency in sorted_money:
+            money = currency[1]['item'].name
+            money_value = self.money[money]['item'].value
+            # Calculate the number of this type of money needed to reach the desired amount
+            num_needed = (amount - total_paid) // money_value
+            # If there are enough of this type of money, pay them all
+            if num_needed <= self.money[money]['count']:
+                total_paid += num_needed * money_value
+                target_wallet.add_money(self.money[money]['item'], num_needed)
+                self.money[money]['count'] -= num_needed
+            # If there are not enough of this type of money, pay as many as possible
+            else:
+                total_paid += self.money[money]['count'] * money_value
+                target_wallet.add_money(self.money[money]['item'], self.money[money]['count'])
+                # del self.money[money]
+            # If the desired amount has been reached, break out of the loop
+            if total_paid == amount:
+                self.add_money(money_to_break)
+                target_wallet.remove_money(money_to_break)
+                break
+       
